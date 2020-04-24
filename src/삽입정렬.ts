01a6654ac13993dde7b2856b2c6ed89c9d23ddae
bbaktaeho <lth9036@gmail.com>
2020-04-24 11:35:46 +0900

    [study] insertion sort 1

---
+++ src/삽입정렬.ts
@@ -0,0 +1,20 @@
+/**
+ * * 삽입 정렬
+ * * 두 번째 데이터부터 시작해서 앞의 데이터와 비교(반복)
+ * * O(n^2)
+ */
+export {};
+
+function insertionSort(arr: Array<number>) {
+  for (let i = 1; i < arr.length; i++) {
+    for (let j = 1; j <= i; j++) {
+      if (arr[i - j + 1] < arr[i - j])
+        [arr[i - j + 1], arr[i - j]] = [arr[i - j], arr[i - j + 1]];
+      else break;
+    }
+  }
+}
+
+const arr = [5, 7, 1, 3, 2, 8, 0, 9, 10, 35, 23, 87, 54];
+insertionSort(arr);
+console.log(arr);
