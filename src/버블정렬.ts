fcd69760f3f16183243cbd8887b80b4f5ce24420
bbaktaeho <lth9036@gmail.com>
2020-06-12 20:19:19 +0900

    정리

--- src/버블정렬.ts
+++ src/버블정렬.ts
@@ -1,20 +1,23 @@
 /**
  * * 버블정렬
+ * todo: 서로 인접한 두 요소를 검사하여 정렬
+ * todo: (오름차순) 한 번 루프를 돌고나면 가장 큰 요소가 맨 뒤로 이동함
+ * todo: 2회전에서 맨 끝에 있는 요소는 정렬에서 제외함
  * ? 스왑이 한 번도 일어나지 않을 때 작성하기
  * * O(n^2)
  */
 export {};
 
 function bubbleSort(arr: Array<number>) {
-  let swapCheck = 0;
-  for (let j = arr.length - 1; j > 0; j--) {
-    for (let i = 0; i < j; i++)
-      if (arr[i] > arr[i + 1]) {
-        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
-        swapCheck++;
-      }
-    if (swapCheck == 0) return;
-  }
+    let swapCheck = 0; // 스왑이 일어났는지 체크
+    for (let j = arr.length - 1; j > 0; j--) {
+        for (let i = 0; i < j; i++)
+            if (arr[i] > arr[i + 1]) {
+                [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+                swapCheck++;
+            }
+        if (swapCheck == 0) return;
+    }
 }
 
 const arr = [1, 2, 3, 4, 5, 6];
--- src/삽입정렬.ts
+++ src/삽입정렬.ts
@@ -1,18 +1,18 @@
 /**
  * * 삽입 정렬
- * * 두 번째 데이터부터 시작해서 앞의 데이터와 비교(반복)
+ * todo: 두 번째 데이터부터 시작해서 앞의 데이터와 비교하여 두 번째 데이터가 작다면 앞 자리로 삽입
+ * todo: 모든 요소를 앞에서부터 차례대로 이미 정렬된 배열 부분과 비교하여 삽입할 위치를 찾음
  * * O(n^2)
  */
 export {};
 
 async function insertionSort(arr: Array<number>): Promise<void> {
-  for (let i = 1; i < arr.length; i++) {
-    for (let j = 1; j <= i; j++) {
-      if (arr[i - j + 1] < arr[i - j])
-        [arr[i - j + 1], arr[i - j]] = [arr[i - j], arr[i - j + 1]];
-      else break;
+    for (let i = 1; i < arr.length; i++) {
+        for (let j = 1; j <= i; j++) {
+            if (arr[i - j + 1] < arr[i - j]) [arr[i - j + 1], arr[i - j]] = [arr[i - j], arr[i - j + 1]];
+            else break;
+        }
     }
-  }
 }
 
 const arr = [5, 7, 1, 3, 2, 8, 0, 9, 10, 35, 23, 87, 54];
